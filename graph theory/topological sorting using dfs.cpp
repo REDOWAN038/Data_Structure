1f31b4e420720d56111fb87e5cc1e355120e9251
REDOWAN038 <redowanibrahim2724@gmail.com>
2021-03-08 21:01:26 +0600

    topo sort 1 added

--- graph theory/topological sorting using dfs.cpp
+++ graph theory/topological sorting using dfs.cpp
@@ -1,22 +1,26 @@
+/// Time Complexity ----> O(V+E)   [V = vertices and E = edges]
+
 #include<bits/stdc++.h>
 using namespace std;
 
-vector <int> graph[100];
-bool visited[100];
+#define sz 100
+vector <int> graph[sz];
+bool visited[sz];
 vector <int> result;
 
 void dfs(int source)
 {
-    visited[source] = 1;
+    visited[source] = true;
     for(int i=0;i<graph[source].size();i++)
     {
         int next = graph[source][i];
-        if(visited[next]==0)
+        if(!visited[next])
             dfs(next);
     }
 
     result.push_back(source);
 }
+
 int main()
 {
     int nodes,edges,i;
@@ -26,18 +30,18 @@ int main()
     {
         int u,v;
         cin>>u>>v;
-        graph[u].push_back(v);
+        graph[u].push_back(v);      /// directed graph
     }
 
-    for(i=0;i<nodes;i++)
+    for(i=1;i<=nodes;i++)
     {
-        if(visited[i]==0)
+        if(!visited[i])
             dfs(i);
     }
 
     reverse(result.begin(),result.end());
     cout<<"Topological sort : ";
-    for(i=0;i<result.size()-1;i++)
+    for(i=0;i<result.size();i++)
         cout<<result[i]<<" ";
     cout<<endl;
 
