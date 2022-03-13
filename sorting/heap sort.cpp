227e6242a555bc16ba7a01543a7c0ac765f91219
REDOWAN038 <redowanibrahim2724@gmail.com>
2022-03-13 20:50:04 +0600

    heap updated

--- sorting/heap sort.cpp
+++ sorting/heap sort.cpp
@@ -1,66 +1,76 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-void inserting_val(vector <int> &heap,int child)
+void insert(vector <int> &heap,int child)
 {
-    int parent = child/2;       /// parent_root_index  = child_root_index/2
+    int parent = child/2;
+
 
     while(heap[parent]<heap[child] && parent>0)
     {
         swap(heap[parent],heap[child]);
         child = parent;
-        parent = child/2;
+        parent/=2;
     }
+
 }
 
-void rearrange(vector <int> &heap)
+void heapify(vector <int> &heap)
 {
     int n = heap.size();
     int parent = 1;
-    int max_val_idx = 1;        /// maximum value index of a tree left or right child
-    int left,right;
+    int idx = 1;
 
-    while(max_val_idx<=n && heap[parent]<=heap[max_val_idx])
+    while(idx<n && heap[parent]<=heap[idx])
     {
-        swap(heap[max_val_idx],heap[parent]);
-        parent = max_val_idx;
-        left = (2*max_val_idx);     /// left child of parent
-        right = (2*max_val_idx)+1;      /// right child of parent
+        swap(heap[parent],heap[idx]);
+        int left = 2*idx;
+        int right = left+1;
+        parent = idx;
 
-        if(heap[left]>heap[right])
-            max_val_idx = left;
+        if(left>=n)
+            break;
+        else if(right>=n)
+            idx = left;
         else
-            max_val_idx = right;
+        {
+            if(heap[left]>heap[right])
+                idx = left;
+            else
+                idx = right;
+        }
     }
+}
+
+void pop(vector <int> &heap)
+{
+    cout<<"Popped element : "<<heap[1]<<endl;
+    int n = heap.size();
+    swap(heap[1],heap[n-1]);
+    heap.pop_back();
+    heapify(heap);
 }
 
 int main()
 {
-    int n;      /// array size
+    int n;
     cin>>n;
-    vector <int> heap(n+1);
-    heap[0] = -1;
+    vector <int> heap;
+
+    heap.push_back(-1);
 
     for(int i=1; i<=n; i++)
     {
-        cin>>heap[i];
-        inserting_val(heap,i);
+        int val;
+        cin>>val;
+        heap.push_back(val);
+        insert(heap,i);
     }
-
-    /// printing heap
-
-    cout<<"Heap Sort : ";
 
     while(heap.size()>1)
     {
-        cout<<heap[1]<<" ";        /// printing root value
-        swap(heap[1],heap[heap.size()]);     /// taking root value to the last position
-
-        auto it = heap.end();
-        it--;
-        heap.erase(it);        /// deleting root value
-
-        rearrange(heap);       /// rearrange the heap after deleting root
+        pop(heap);
     }
+
+    return 0;
 }
